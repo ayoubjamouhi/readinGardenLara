f7d73bcb880495cad2b8e73122aa49e45f47844b
jfnadev <gemouhi@gmail.com>
2019-10-29 19:28:29 +0900

    Replace postby nodejs post css grid

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,62 +1,73 @@
 @section('title',  $post->title )
 @extends('layouts.app')
 @section('css')
-    <link rel="stylesheet" href="css/article.css" >
+    <link rel="stylesheet" href="css/post-grid.css" >
 @endsection
 @section('main')
-<div class="container">
    <section class="post">
-         @if($post->largeImage != null)
-         <div class="image--top">
-               <img src={{ $post->largeImage }} alt="img" />
-               <p class="credit">Credit: {{ $post->credit }}</p>
-         </div>
-         @endif
-         <div class="row bottom">
-               <div class="col-12 col-sm-1 col-md-1 col-lg-1 col-xl-1 left">
-                  <div class="share">
-                     <a href="#">
-                        <i class="fab fa-facebook"></i>
-                     </a>
-                     <a href="#">
-                        <i class="fab fa-twitter"></i>
-                     </a>
-                  </div>
-               </div>
-               <div class="col-12 col-sm-11 col-md-11 col-lg-11 col-xl-11 right">
-                  <div>Ad</div>
-                  <h3 class="categorie">IN {{ $post->categorie }}</h3>
-                  <h1 class="title">{{ $post->title }}</h1><a href={{"/post_update/" . $post->id }}>🖊</a>
-                  <h2 class="description">{{ $post->descreption }}</h2>
-                  <div class="user">
-                     <div class="row">
-                           <div class="col-12 col-sm-11 col-md-11 col-lg-11 col-xl-11 image">
-                              <img src="/images/admin.png" alt="images" class="img-circle"/>
-                           </div>
-                           <div class="col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 info">
-                              <div class="name--follow">
-                                 <h6>{{ $post->user_id }}</h6>
-                                 <button>Follow</button>
-                              </div>
-                              <div class="date">
-                              {{ $post->created_at->format('d/m/Y') }}
-                              </div>
-
-                           </div>
-                     </div>
-                  </div>
-                  <div class="row article">
-                     <div class="col-12 col-sm-9 col-md-9 col-lg-9 col-xl-9 col" >
-                        {!! $post->html !!}
-                     </div>
-                     <div  class="col-12 col-sm-3 col-md-3 col-lg-3 col-xl-3 col">
-                        <div>Ad</div>
-                     </div>
-                  </div>
-
-               </div>
-         </div>
-      </section>
-</div>
+   <div class="myads">
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+      <!-- ad unit 1 -->
+      <ins class="adsbygoogle"
+         style="display:block"
+         data-ad-client="ca-pub-3080286913683536"
+         data-ad-slot="2002740794"
+         data-ad-format="auto"
+         data-full-width-responsive="true"></ins>
+      <script>
+         (adsbygoogle = window.adsbygoogle || []).push({});
+      </script>
+   </div>
+   <figure class="image">
+   @if($post->largeImage != null)
+      <img src={{ $post->largeImage }} alt={{ $post->title }} />
+   @endif
+      <figcaption>
+         {{ $post->credit }}
+      </figcaption>
+   </figure>
+   <div class="user__categorie">
+      <div class="user">
+         <img src="/images/admin.png" alt="images" class="img-circle"/>
+         <a href={{ "/user/" . $post->user_id }}>
+            {{ $post->user_id }}
+         </a>
+      </div>
+      <div class="categorie">
+         <a href={{ "/categorie/" . $post->categorie }}>
+            {{ ucfirst(trans($post->categorie)) }}
+         </a>
+      </div>
+   </div>
+   <h1 class="title">
+      {{ $post->title }}
+      <a href={{"/post_update/" . $post->id }}>🖊</a>
+   </h1>
+   <span class="date">
+      <time>
+         {{ $post->created_at->format('F Y') }}
+      </time>
+   </span>
+   <article
+      id="article"
+   >{!! $post->html !!}
+   </article>
+   <div class="myads">
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+      <!-- ad unit 1 -->
+      <ins class="adsbygoogle"
+         style="display:block"
+         data-ad-client="ca-pub-3080286913683536"
+         data-ad-slot="2002740794"
+         data-ad-format="auto"
+         data-full-width-responsive="true"></ins>
+      <script>
+         (adsbygoogle = window.adsbygoogle || []).push({});
+      </script>
+   </div>
+   <div class="disqus">
+      Disqus
+   </div>
+   </section>
 @endsection
 
